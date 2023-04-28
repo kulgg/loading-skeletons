d2a0ecbb4d50d0fee8cbb971e6b932b0031f73d0
kulgg <75735874+kulgg@users.noreply.github.com>
2023-04-28 20:42:24 +0200

    Force server side rendering

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,3 +1,6 @@
+import { randomInt } from "crypto";
+import { sleep } from "./utils";
+
 export interface StatsType {
   title: string;
   stat: string;
@@ -5,9 +8,44 @@ export interface StatsType {
 }
 
 export async function fetchStats(type: string): Promise<StatsType> {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/data/${type}`,
-    { cache: "no-cache" }
-  );
-  return await response.json();
+  await sleep(randomInt(1500));
+
+  // const a = await fetch("/api/yo", {
+  //   cache: "no-cache",
+  // });
+
+  if (type === "revenue") {
+    return {
+      title: "Total Revenue",
+      stat: "$45,231.89",
+      subline: "+20.1% from last month",
+    };
+  }
+  if (type === "subscriptions") {
+    return {
+      title: "Subscriptions",
+      stat: "+2350",
+      subline: "+180.1% from last month",
+    };
+  }
+  if (type === "sales") {
+    return {
+      title: "Sales",
+      stat: "+12,234",
+      subline: "+19% from last month",
+    };
+  }
+  if (type === "active") {
+    return {
+      title: "Active Now",
+      stat: "+573",
+      subline: "+201 since last hour",
+    };
+  }
+
+  return {
+    title: "",
+    stat: "",
+    subline: "",
+  };
 }
