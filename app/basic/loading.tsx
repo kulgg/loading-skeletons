715cb72dc2ef0768d423f40bbb2fe2cbc1179619
kulgg <75735874+kulgg@users.noreply.github.com>
2023-04-28 19:45:02 +0200

    Add cool looking skeleton

--- app/basic/loading.tsx
+++ app/basic/loading.tsx
@@ -1,7 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="grid grid-cols-2 gap-6">
+      <Skeleton className="w-full h-32" />
+      <Skeleton className="w-full h-32" />
+      <Skeleton className="w-full h-32" />
+      <Skeleton className="w-full h-32" />
+    </div>
+  );
 }
 
 export default Loading;
